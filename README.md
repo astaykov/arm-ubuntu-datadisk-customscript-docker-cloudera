1a085bf04720c28e444adbfb0abc6aeb0c82e876
Anton Staykov <astaykov@gmail.com>
2018-05-03 23:20:50 +0200

    test

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # arm-ubuntu-datadisk-customscript-docker-cloudera
 A quick start template that installs Ubuntu, adds &amp; initializes data disk, installs docker, downloads Cloudera quick start docker image, imports the image into docker
+
+https://raw.githubusercontent.com/astaykov/arm-ubuntu-datadisk-customscript-docker-cloudera/master
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fastaykov%2Farm-ubuntu-datadisk-customscript-docker-cloudera%2Fmaster%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fastaykov%2Farm-ubuntu-datadisk-customscript-docker-cloudera%2Fmaster%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-echo 'hello'
+# This script is necessary to set up the data disk that will 
+# keep the docker image and serve as mount point
+
+# The ARM script only mounts a single data disk.  It is safe 
+# to assume that on a new VM, this data disk is located at /dev/sdc.
+
+# If you have a more complicated setup, then do examine what this
+# script is doing and modify accordingly.
+
+# create a partition table for the disk
+parted -s /dev/sdc mklabel msdos
+
+# create a single large partition
+parted -s /dev/sdc mkpart primary ext4 0\% 100\%
+
+# install the file system
+mkfs.ext4 /dev/sdc1
+
+# create the mount point
+mkdir /datadisk
+
+# mount the disk
+sudo mount /dev/sdc1 /datadisk/
+
+# add mount to /etc/fstab to persist across reboots
+echo "/dev/sdc1    /datadisk/    ext4    defaults 0 0" >> /etc/fstab
+
+# install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# install wget
+sudo apt-get install wget
+
+#get cloudera image
+cd /datadisk
+sudo wget https://downloads.cloudera.com/demo_vm/docker/cloudera-quickstart-vm-5.13.0-0-beta-docker.tar.gz
+sudo tar -zxvf cloudera-quickstart-vm-5.13.0-0-beta-docker.tar.gz .
+docker import - cloudera/cloudera:latest < /datadisk/cloudera-quickstart-vm-*-docker*.tar
+
+
